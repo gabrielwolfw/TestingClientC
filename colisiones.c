8ba9e2d973c63aafda316c36cb40f050a7a362a4
Gabriel Lobo <kevinlobo1920k@gmail.com>
2023-04-19 10:04:25 -0600

    funcion de colisiones, se añadio en colisiones.c

---
+++ colisiones.c
@@ -0,0 +1,12 @@
+#include "raylib.h"
+#include "stdbool.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct{
+    Rectangle naveBox;
+    Rectangle ovniBox;
+    Rectangle invasorBox;
+    Rectangle disparoBox;
+}Colisiones;
+
